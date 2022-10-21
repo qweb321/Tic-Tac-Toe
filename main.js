b9f1a4070c7660a198ee1050ae6705f54b7d2ae3
Isis Lin <qweb32114@gmail.com>
2022-10-21 11:41:16 +0800

    Add win condition

--- main.js
+++ main.js
@@ -1,31 +1,65 @@
-let turn = 'circle'
-const playData = {
-    circle:[],
-    cross:[],
-}
-function draw(target, symbol){
-    target.classList.add(symbol)
+let turn = "circle";
+const playerData = {
+  circle: [],
+  cross: [],
+};
+
+const winCondition = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [2, 5, 8],
+  [1, 4, 7],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+function draw(target, symbol) {
+  target.innerHTML = `<p class=${symbol}></p>`;
 }
 
-function recordPlay(player, index){
-    playData[player].push(index)
+function recordPlay(player, index) {
+  playerData[player].push(index);
 }
 
-document.querySelector('.app').addEventListener("click", function onTableClicked(event) {
-
-    if(event.target.tagName !== 'TD'){
-        return
+function checkWinCon(playerPosition) {
+  for (const con of winCondition) {
+    if (con.every((position) => playerPosition.includes(position))) {
+      return true;
     }
-    
-    if(turn === 'circle'){
-        draw(event.target, 'circle')
-        recordPlay('circle', Number(event.target.dataset.index))
-        turn = 'cross'
-    } else if ( turn === 'cross') {
-        draw(event.target, 'cross')
-        recordPlay('cross', Number(event.target.dataset.index))
-        turn = 'circle'
+  }
+
+  return false;
+}
+
+function whoWins(player) {
+  if (checkWinCon(playerData[player])) {
+    removeEventListener();
+    return alert(`${player} player is win`);
+  }
+}
+
+function removeEventListener() {
+  document.querySelector(".app").removeEventListener("click", onTableClicked);
+}
+
+function onTableClicked(event) {
+  if (event.target.tagName === "TD") {
+    if (turn === "circle") {
+      draw(event.target, "circle");
+      recordPlay("circle", Number(event.target.dataset.index));
+      whoWins("circle");
+      turn = "cross";
+    } else if (turn === "cross") {
+      draw(event.target, "cross");
+      recordPlay("cross", Number(event.target.dataset.index));
+      whoWins("cross");
+      turn = "circle";
     }
-    console.log(event.target)
-    console.log(playData)
-})
+  } else {
+    return alert("This postion already has content!");
+  }
+}
+
+document.querySelector(".app").addEventListener("click", onTableClicked);
